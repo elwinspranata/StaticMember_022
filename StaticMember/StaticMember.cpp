913a7bb30088a74df609a15945a70c2f43d20cb6
elwinspranata <elwinpranata02@gmail.com>
2023-05-17 15:30:11 +0700

    Member

--- StaticMember/StaticMember.cpp
+++ StaticMember/StaticMember.cpp
@@ -3,10 +3,13 @@
 using namespace std; 
 
 class mahasiswa {
-public: 
-	static int nim; 
+public:
+	static int nim;
 	int id;
-	string nama; 
+	string nama;
 
-
+	void setID();
+	void printAll();
+	mahasiswa(string pnama) :nama(pnama) { setID(); }
 };
+
