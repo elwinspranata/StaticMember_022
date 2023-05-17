5103a567c263696acda33adeeeb56b33f6a2eb75
elwinspranata <elwinpranata02@gmail.com>
2023-05-17 15:42:12 +0700

    main

--- StaticMember/StaticMember.cpp
+++ StaticMember/StaticMember.cpp
@@ -29,10 +29,14 @@ void mahasiswa::printAll() {
 int main() {
 	mahasiswa mhs1("Lia Kurnia");
 	mahasiswa mhs2("Asroni");
-	mhs2.nim = 10; 
+	mhs2.nim = 10;
 	mahasiswa mhs3("Andi Kurniawan");
-	mhs3.nim = 20; 
+	mhs3.nim = 20;
 	mahasiswa mhs4("Joko Purbo");
 
-
+	mhs1.printAll();
+	mhs2.printAll();
+	mhs3.printAll();
+	mhs4.printAll();
+	return 0;
 }
